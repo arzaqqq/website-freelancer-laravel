f724671a1e94f99e56c878c10aa7b7b5388d6564
Arzaqqq <mmiftahularzaq01@gmail.com>
2025-03-04 09:07:44 +0700

    FIX :Folder view salah ,pindah semua isi folder view ke folder view di resource

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 
-// 
+// FRONT {Landing}
+use App\Http\Controllers\Landing\LandingController;
+
+
+
+// MEMBER {Dashboard}
 
 
 
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified',
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+
+Route::resource('/', LandingController::class);
